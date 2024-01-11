021018c97410eb99e0b053647f19e73ef6b42e28
serin-B <bangserin980302@gmail.com>
2024-01-11 23:42:24 +0900

    feat: 빠른 추가 재료들 상수화

---
+++ src/constants/QuickAdd.ts
@@ -0,0 +1,31 @@
+const QUICK_ADD_TO_KOR = Object.freeze({
+  BUTTER: "버터",
+  SUGAR: "설탕",
+  SALT: "소금",
+  EGG: "달걀",
+  BREAD_FLOUR: "강력분",
+  MEDIUM_FLOUR: "중력분",
+  SOFT_FLOUR: "박력분",
+  BAKING_POWDER: "베이킹파우더",
+  YEAST: "이스트",
+  MILK: "우유",
+  WATER: "물",
+  VANILLA_EXTRACT: "바닐라익스트랙",
+});
+
+const QUICK_ADD_TO_ENG = Object.freeze({
+  버터: "BUTTER",
+  설탕: "SUGAR",
+  소금: "SALT",
+  달걀: "EGG",
+  강력분: "BREAD_FLOUR",
+  중력분: "MEDIUM_FLOUR",
+  박력분: "SOFT_FLOUR",
+  베이킹파우더: "BAKING_POWDER",
+  이스트: "YEAST",
+  우유: "MILK",
+  물: "WATER",
+  바닐라익스트랙: "VANILLA_EXTRACT",
+});
+
+export { QUICK_ADD_TO_ENG, QUICK_ADD_TO_KOR };
